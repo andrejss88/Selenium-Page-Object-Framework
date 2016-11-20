26598ae4be783ed6e6f1d0b61987283c6bca8272
Andrejs <andrey.doronins@hotmail.com>
2016-11-20 13:00:06 +0100

    Update to Selenium 3

--- src/test/java/com/github/setup/SeleniumDriver.java
+++ src/test/java/com/github/setup/SeleniumDriver.java
@@ -14,6 +14,8 @@ public class SeleniumDriver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+            // Selenium 3 needs additional prop setting, see http://stackoverflow.com/questions/37785686/how-to-use-the-gecko-executable-with-selenium
+            System.setProperty("webdriver.firefox.marionette", "C:\\Program Files\\Mozilla Firefox\\geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
             driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             return driver;
